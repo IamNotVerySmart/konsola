ab093e4a76e84eaf3ffec121ffe44caa607bf19a
Kacper Chrabąszcz <kacper.chrabaszcz@teb.edu.pl>
2024-03-12 23:25:10 +0100

    Add project files.

---
+++ konsola/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+Console.WriteLine("a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("b: ");
+int b = int.Parse(Console.ReadLine());
+int nwd(int x, int y)
+{
+    while (y != 0)
+    {
+        if (x > y) x = x % y;
+        else y = y % x;
+    }
+    return y;
+}
+Console.WriteLine("a= " + nwd(a, b));
+/**********************************************
+nazwa funkcji: nwd
+opis funkcji: wyznacza najwiekszy wspolny dzielnik
+parametry: nwm ocb
+zwracany typ i opis: int, najwiekszy wspolny dzielnik?
+autor: 69420213769
+***********************************************/
